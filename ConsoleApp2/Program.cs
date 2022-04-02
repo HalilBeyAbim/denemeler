a9383c297b64cd0d40fd4af04d58406e7d8e944c
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-02 18:22:19 +0400

    added new console and JPG file

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 10;
+            int b = 5;
+            int c = 10;
+            int d = 5;
+            int e = 3;
+            
+            double result =  (double)(a + b + c + d + e) / 5;
+            Console.WriteLine(result);
+        }
+    }
+}
